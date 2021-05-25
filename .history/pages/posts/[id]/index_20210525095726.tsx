e2987a92378e1d48a3edfb3467ae3b08457ba032
rushilramesh <rushilramesh@gmail.com>
2021-05-25 10:01:50 +0800

    reverting back to old version

--- .history/pages/posts/[id]/index_20210525095726.tsx
+++ .history/pages/posts/[id]/index_20210525095726.tsx
@@ -190,7 +190,7 @@ const Post = (
 export default Post;
 
 export const getStaticPaths : GetStaticPaths = async () => {
-    const res = await fetch(`${server}/api/users`)
+    const res = await fetch(`http://localhost:3000//api/users`)
     const posts = await res.json()
     const paths = posts.map(post => {
         return {
